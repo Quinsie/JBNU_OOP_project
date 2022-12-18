d4e64394cf15dbfb28fbe3b5c230e4692ee8c9e2
Jiho <jyjh0501@naver.com>
2022-12-18 17:34:20 +0900

    Restart base making 02

--- Timetable_Supporter/src/main_pkg/Frame.java
+++ Timetable_Supporter/src/main_pkg/Frame.java
@@ -2,8 +2,10 @@ package main_pkg;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 
 public class Frame extends JFrame
@@ -14,9 +16,11 @@ public class Frame extends JFrame
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setResizable(false);
 		this.setLocationRelativeTo(null);
+		this.setLayout(null);
 		
 		createMenu();
 		createTimetable();
+		createTimetableTitle();
 		
 		this.setVisible(true);
 	}
@@ -27,7 +31,17 @@ public class Frame extends JFrame
 		JMenu editMenu = new JMenu();
 		
 		fileMenu.setText("File");
+		fileMenu.add(new JMenuItem("1"));
+		fileMenu.add(new JMenuItem("2"));
+		fileMenu.add(new JMenuItem("3"));
+		fileMenu.addSeparator();
+		
 		editMenu.setText("Edit");
+		editMenu.add(new JMenuItem("1"));
+		editMenu.add(new JMenuItem("2"));
+		editMenu.add(new JMenuItem("3"));
+		editMenu.addSeparator();
+		
 		mb.add(fileMenu);
 		mb.add(editMenu);
 		
@@ -36,13 +50,27 @@ public class Frame extends JFrame
 	
 	public void createTimetable() {
 		JPanel timetable = new JPanel();
+		timetable.setBounds(30, 70, 575, 560);
 		timetable.setLayout(null);
 		
 		JButton timetableButton = new JButton();
 		timetableButton.setText("시간표 수정 (시간표 추가 예정)");
 		timetable.add(timetableButton);
-		timetableButton.setBounds(30, 75, 575, 555);
+		timetableButton.setBounds(0, 0, 575, 560);
 		
 		this.add(timetable);
+	}
+	
+	public void createTimetableTitle() {
+		JPanel title = new JPanel();
+		title.setBounds(30, 20, 200, 20);
+		title.setLayout(null);
+		
+		JLabel text = new JLabel();
+		text.setText("시간표");
+		title.add(text);
+		text.setBounds(0, 0, 200, 20);
+		
+		this.add(title);
 	}
 }
